d6a5076cc2ddda412cc9f69bc8ad81574bbc898f
Kamran Iqbal <developerkamraniqbal@gmail.com>
2024-06-26 15:29:11 +0500

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+import './globals.css';
+
+const roboto = Roboto({ subsets: ['latin'], weight: '400' });
+
+export const metadata: Metadata = {
+  title: 'Expense Tracker',
+  description: 'Track your expenses and create a budget',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
